3a6395efb1e508b41a6f63144ee5f812b3463567
nikhea <imonikheaugbodaga@gmail.com>
2023-01-26 20:47:55 +0100

    added Global styles

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -2,7 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;700&family=Roboto:ital,wght@0,100;0,500;1,100;1,300;1,400;1,500;1,700&display=swap');
-
 html,
 body {
     padding: 0;
